0956c753607e88e36285f204a20c0794ec91fff0
Austin Bergstrom <awbergs@gmail.com>
2017-01-28 20:37:42 -0500

    recaptcha

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,13 @@ class ApplicationController < ActionController::Base
 
   def save
     ip_address = request.remote_ip
-    if Submission.can_submit?(ip_address)
-      flash[:notice] = 'Saved'
-      Submission.create(approval: params[:approval] == "1", ip_address: request.remote_ip)
+    if true #Submission.can_submit?(ip_address)
+      if validate_captcha
+        flash[:notice] = 'Saved'
+        Submission.create(approval: params[:approval] == "1", ip_address: request.remote_ip)
+      else
+        flash[:notice] = 'Invalid captcha'
+      end
     else
       flash[:notice] = 'You must wait at least 24 hours between submissions.'
     end
@@ -26,5 +30,19 @@ class ApplicationController < ActionController::Base
     else
       @approval_ratio = 0
     end
+  end
+
+  def validate_captcha
+    response = HTTParty.post('https://www.google.com/recaptcha/api/siteverify', {
+      body: {
+        secret: '6LfCfRMUAAAAAAvqhVLyVaUVYx5cqho_yepO7oT-',
+        response: params["g-recaptcha-response"],
+        remoteip: request.remote_ip
+      }
+    })
+    resp = JSON.parse response.body
+    p resp
+    p resp["success"]
+    return resp["success"]
   end
 end
